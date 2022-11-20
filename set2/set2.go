ffbe72b20c80ffbecef66f96ba0cc94545f1d7a8
Lisogubenko Vitaliy <vlisogubenko@wimark.com>
2022-11-20 09:00:01 +0400

    set-2 : Implement CBC mode (complete)

--- set2/set2.go
+++ set2/set2.go
@@ -1,6 +1,9 @@
 package set2
 
-import "bytes"
+import (
+	"bytes"
+	"crypto/cipher"
+)
 
 func paddingPKCS7(b []byte, size int) []byte {
 	paddingLen := size - (len(b) % size)
@@ -15,4 +18,56 @@ func paddingPKCS7(b []byte, size int) []byte {
 	}
 
 	return buf.Bytes()
+}
+
+func encryptedCBC(plaintText []byte, block cipher.Block, iv []byte) []byte {
+	if len(plaintText)%block.BlockSize() != 0 {
+		panic("invalid input length")
+	}
+	if len(iv) != block.BlockSize() {
+		panic("invalid iv length")
+	}
+
+	cipherText := make([]byte, len(plaintText))
+	prev := iv
+	bs := block.BlockSize()
+	n := len(plaintText) / bs
+	for i := 0; i < n; i++ {
+		block.Encrypt(cipherText[i*bs:(i+1)*bs], xorBytes(plaintText[:bs], prev))
+		prev = cipherText[i*bs : (i+1)*bs]
+		plaintText = plaintText[bs:]
+	}
+
+	return cipherText
+}
+
+func decryptedCBC(cipherText []byte, block cipher.Block, iv []byte) []byte {
+	if len(cipherText)%block.BlockSize() != 0 {
+		panic("invalid input length")
+	}
+	if len(iv) != block.BlockSize() {
+		panic("invalid iv length")
+	}
+
+	plainText := make([]byte, len(cipherText))
+	bs := block.BlockSize()
+	n := len(cipherText) / bs
+	prev := iv
+	for i := 0; i < n; i++ {
+		block.Decrypt(plainText[i*bs:(i+1)*bs], cipherText[i*bs:])
+		copy(plainText[i*bs:(i+1)*bs], xorBytes(plainText[i*bs:(i+1)*bs], prev))
+		prev = cipherText[i*bs : (i+1)*bs]
+	}
+	return plainText
+}
+
+func xorBytes(a, b []byte) []byte {
+	if len(a) > len(b) {
+		a = a[:len(b)]
+	}
+	var out = make([]byte, len(a))
+	for i := 0; i < len(out); i++ {
+		out[i] = a[i] ^ b[i]
+	}
+	return out
 }
--- set2/set2_test.go
+++ set2/set2_test.go
@@ -2,14 +2,48 @@ package set2
 
 import (
 	"bytes"
+	"crypto/aes"
+	"encoding/base64"
+	"io/ioutil"
 	"testing"
 )
 
-func TestChallengeSet(t *testing.T) {
+func TestSet2Challenge9(t *testing.T) {
 	in := []byte("YELLOW SUBMARINE")
 	out := []byte("YELLOW SUBMARINE\x04\x04\x04\x04")
 	res := paddingPKCS7(in, 20)
 	if !bytes.Equal(res, out) {
 		t.Fatalf("wrongs res ex: %s got: %s\n", out, res)
 	}
+}
+
+func TestSet2Challenge10(t *testing.T) {
+	plaintText := []byte("YELLOW SUBMARINEYELLOW SUBMARINE")
+	iv := make([]byte, 16)
+	b, _ := aes.NewCipher([]byte("YELLOW SUBMARINE"))
+	res := decryptedCBC(encryptedCBC(plaintText, b, iv), b, iv)
+	if !bytes.Equal(plaintText, res) {
+		t.Errorf("invalid result %s\n", string(res))
+	}
+
+	data := decodeBase64(t, string(readFile(t, "./challengedata/challenge10.txt")))
+	t.Logf("decrypted res:\n%s", decryptedCBC(data, b, iv))
+}
+
+func decodeBase64(t *testing.T, s string) []byte {
+	t.Helper()
+	v, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		t.Fatalf("failed to decode base64: %s\n", s)
+	}
+	return v
+}
+
+func readFile(t *testing.T, filename string) []byte {
+	t.Helper()
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Fatalf("failed open file %s with err %s\n", filename, err.Error())
+	}
+	return data
 }
